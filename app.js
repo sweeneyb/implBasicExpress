721377c35f4d7c9bf3139d60cbc53a1ae85abcd4
sweeneyb <tab126@gmail.com>
2019-10-22 08:04:15 -0500

    very simple printing of the command

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var readline = require('readline'),
   rl = readline.createInterface(process.stdin, process.stdout),
-  prefix = 'OHAI> ';
+  prefix = '> ';
 
 rl.on('line', function(line) {
     var split = line.split(" ")
-    switch ( split[0].trim ) {
+    switch ( split[0].trim() ) {
         case 'GET':
           console.log( "get ", split[1])
           break;
@@ -14,23 +14,11 @@ rl.on('line', function(line) {
         default:
           console.log("unknown command")
     }
-    // split.forEach(element => {
-    //     console.log(element)
-    // });
-//   switch(line.trim()) {
-//     case 'hello':
-//       console.log('world!');
-//       break;
-//     default:
-//       console.log('Say what? I might have heard `' + line.trim() + '`');
-//       break;
-//   }
   rl.setPrompt(prefix, prefix.length);
   rl.prompt();
 }).on('close', function() {
-  console.log('Have a great day!');
   process.exit(0);
 });
-console.log(prefix + 'Good to see you. Try typing stuff.');
+// console.log(prefix);
 rl.setPrompt(prefix, prefix.length);
 rl.prompt();
