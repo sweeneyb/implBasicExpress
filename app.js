32005fa38af163311100212d55af4a068c3389b1
sweeneyb <tab126@gmail.com>
2019-10-24 14:59:04 -0500

    moved out request handling to its own method

--- app.js
+++ app.js
@@ -1,50 +1,55 @@
 var readline = require('readline'),
-  rl = readline.createInterface(process.stdin, process.stdout),
-  prefix = 'FOO> ';
+    rl = readline.createInterface(process.stdin, process.stdout),
+    prefix = '> ';
 
 
-  // derived from https://gist.github.com/initlove/2478016
-rl.on('line', function(line) {
-  doStuff(line)
-  rl.setPrompt(prefix, prefix.length);
-  rl.prompt();
-}).on('close', function() {
-  process.exit(0);
+// derived from https://gist.github.com/initlove/2478016
+rl.on('line', function (line) {
+    doStuff(line)
+    rl.setPrompt(prefix, prefix.length);
+    rl.prompt();
+}).on('close', function () {
+    process.exit(0);
 });
 rl.setPrompt(prefix, prefix.length);
 rl.prompt();
 
 var handlers = {}
 handlers["GET"] = {}
+handlers["PUT"] = {}
 
-function get( path, getFn) {
+function get(path, getFn) {
     let paths = handlers["GET"]
     paths[path] = getFn
 }
-function put(putFn) { 
-    handlers["PUT"] = putFn
+
+function put(path, putFn) {
+    handlers["PUT"][path] = putFn
 }
 
 function doStuff(line) {
     var split = line.split(" ")
-    switch ( split[0].trim() ) {
+    switch (split[0].trim()) {
         case 'GET':
-          let paths = handlers["GET"]
-          var handler = paths[split[1]]
-          if(handler == undefined) {
-              console.log("404 - not found")
-          } else {
-             handler(split[1])
-          }
-          break
-        case 'PUT': 
-          handlers["PUT"](split[1])
-          break
+            deferRequest("GET", split[1])
+            break
+        case 'PUT':
+            deferRequest("PUT", split[1])
+            break
         default:
-          console.log("unknown command")
+            console.log("unknown command")
     }
 }
 
+function deferRequest(method, path) {
+    let paths = handlers[method]
+    var handler = paths[path]
+    if (handler == undefined) {
+        console.log("404 - not found")
+    } else {
+        handler(path)
+    }
+}
 
 // above this comment is "framework" stuff.  It handles common tasks
 // below this comment is configuring the framework & specifying what gets done.
@@ -56,10 +61,10 @@ function doRoot(what) {
 function doGet2(what) {
     console.log("get2\n\n", what);
 }
-get( "/", doRoot)
+get("/", doRoot)
 get("/get2", doGet2)
 
 // this is starting to look a lot like express
-put(function doPut(what) {
+put("/", function doPut(what) {
     console.log("put", what)
 })
